749baf5740901758ff12c43fd8b02e424f8435d4
Jacek <jacekzwroclawia@gmail.com>
2011-08-03 01:34:13 +0200

    Koniec, layout i routes

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -9,4 +9,9 @@ module ApplicationHelper
       "#{base_title} | #{@title}"
     end
   end
+
+  # logo in _layouts/_header.html.erb
+  def logo
+    image_tag("logo.png", :alt => "Sample App", :class => "round")
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,14 @@
 SampleApp::Application.routes.draw do
+  get "users/new"
+
   root :to => 'pages#home'
+  match '/signup', :to =>'users#new'
   match '/contact', :to => 'pages#contact'
   match '/about', :to => 'pages#about'
   match '/help', :to => 'pages#help'
 
 
-  match
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
